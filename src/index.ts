5b5fabf159dee8731d059a3efa043b70d0b7f771
Nils Abegg <nils@nilsabegg.com>
2024-06-20 02:02:54 +0700

    Old functions restored

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,162 @@ export default class PumpFunTrader {
 
         return this;
     }
+    async pumpFunBuy(
+        payerPrivateKey: string,
+        mintStr: string,
+        solIn: number,
+        priorityFeeInSol: number = 0,
+        slippageDecimal: number = 0.25,
+        isSimulation: boolean = true
+    ) {
+        try {
+            const connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed');
 
+            const coinData = await getCoinData(mintStr);
+            if (!coinData) {
+                console.error('Failed to retrieve coin data...');
+                return;
+            }
+
+            const payer = await getKeyPairFromPrivateKey(payerPrivateKey);
+            const owner = payer.publicKey;
+            const mint = new PublicKey(mintStr);
+
+            const txBuilder = new Transaction();
+
+            const tokenAccountAddress = await getAssociatedTokenAddress(mint, owner, false);
+
+            const tokenAccountInfo = await connection.getAccountInfo(tokenAccountAddress);
+
+            let tokenAccount: PublicKey;
+            if (!tokenAccountInfo) {
+                txBuilder.add(createAssociatedTokenAccountInstruction(payer.publicKey, tokenAccountAddress, payer.publicKey, mint));
+                tokenAccount = tokenAccountAddress;
+            } else {
+                tokenAccount = tokenAccountAddress;
+            }
+
+            const solInLamports = solIn * LAMPORTS_PER_SOL;
+            const tokenOut = Math.floor((solInLamports * coinData['virtual_token_reserves']) / coinData['virtual_sol_reserves']);
+
+            const solInWithSlippage = solIn * (1 + slippageDecimal);
+            const maxSolCost = Math.floor(solInWithSlippage * LAMPORTS_PER_SOL);
+            const ASSOCIATED_USER = tokenAccount;
+            const USER = owner;
+            const BONDING_CURVE = new PublicKey(coinData['bonding_curve']);
+            const ASSOCIATED_BONDING_CURVE = new PublicKey(coinData['associated_bonding_curve']);
+
+            const keys = [
+                { pubkey: GLOBAL, isSigner: false, isWritable: false },
+                { pubkey: FEE_RECIPIENT, isSigner: false, isWritable: true },
+                { pubkey: mint, isSigner: false, isWritable: false },
+                { pubkey: BONDING_CURVE, isSigner: false, isWritable: true },
+                { pubkey: ASSOCIATED_BONDING_CURVE, isSigner: false, isWritable: true },
+                { pubkey: ASSOCIATED_USER, isSigner: false, isWritable: true },
+                { pubkey: USER, isSigner: false, isWritable: true },
+                { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+                { pubkey: RENT, isSigner: false, isWritable: false },
+                { pubkey: PUMP_FUN_ACCOUNT, isSigner: false, isWritable: false },
+                { pubkey: PUMP_FUN_PROGRAM, isSigner: false, isWritable: false }
+            ];
+
+            const data = Buffer.concat([bufferFromUInt64('16927863322537952870'), bufferFromUInt64(tokenOut), bufferFromUInt64(maxSolCost)]);
+
+            const instruction = new TransactionInstruction({
+                keys: keys,
+                programId: PUMP_FUN_PROGRAM,
+                data: data
+            });
+            txBuilder.add(instruction);
+
+            const transaction = await createTransaction(connection, txBuilder.instructions, payer.publicKey, priorityFeeInSol);
+            if (!isSimulation) {
+                const signature = await sendTransaction(connection, transaction, [payer]);
+                console.log('Buy transaction confirmed:', signature);
+            } else if (isSimulation) {
+                const simulatedResult = await connection.simulateTransaction(transaction);
+                console.log(simulatedResult);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async pumpFunSell(
+        payerPrivateKey: string,
+        mintStr: string,
+        tokenBalance: number,
+        priorityFeeInSol: number = 0,
+        slippageDecimal: number = 0.25,
+        isSimulation: boolean = true
+    ) {
+        try {
+            const connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed');
+
+            const coinData = await getCoinData(mintStr);
+            if (!coinData) {
+                console.error('Failed to retrieve coin data...');
+                return;
+            }
+
+            const payer = await getKeyPairFromPrivateKey(payerPrivateKey);
+            const owner = payer.publicKey;
+            const mint = new PublicKey(mintStr);
+            const txBuilder = new Transaction();
+
+            const tokenAccountAddress = await getAssociatedTokenAddress(mint, owner, false);
+
+            const tokenAccountInfo = await connection.getAccountInfo(tokenAccountAddress);
+
+            let tokenAccount: PublicKey;
+            if (!tokenAccountInfo) {
+                txBuilder.add(createAssociatedTokenAccountInstruction(payer.publicKey, tokenAccountAddress, payer.publicKey, mint));
+                tokenAccount = tokenAccountAddress;
+            } else {
+                tokenAccount = tokenAccountAddress;
+            }
+
+            const minSolOutput = Math.floor(
+                (tokenBalance! * (1 - slippageDecimal) * coinData['virtual_sol_reserves']) / coinData['virtual_token_reserves']
+            );
+
+            const keys = [
+                { pubkey: GLOBAL, isSigner: false, isWritable: false },
+                { pubkey: FEE_RECIPIENT, isSigner: false, isWritable: true },
+                { pubkey: mint, isSigner: false, isWritable: false },
+                { pubkey: new PublicKey(coinData['bonding_curve']), isSigner: false, isWritable: true },
+                { pubkey: new PublicKey(coinData['associated_bonding_curve']), isSigner: false, isWritable: true },
+                { pubkey: tokenAccount, isSigner: false, isWritable: true },
+                { pubkey: owner, isSigner: false, isWritable: true },
+                { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+                { pubkey: ASSOC_TOKEN_ACC_PROG, isSigner: false, isWritable: false },
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+                { pubkey: PUMP_FUN_ACCOUNT, isSigner: false, isWritable: false },
+                { pubkey: PUMP_FUN_PROGRAM, isSigner: false, isWritable: false }
+            ];
+
+            const data = Buffer.concat([bufferFromUInt64('12502976635542562355'), bufferFromUInt64(tokenBalance), bufferFromUInt64(minSolOutput)]);
+
+            const instruction = new TransactionInstruction({
+                keys: keys,
+                programId: PUMP_FUN_PROGRAM,
+                data: data
+            });
+            txBuilder.add(instruction);
+
+            const transaction = await createTransaction(connection, txBuilder.instructions, payer.publicKey, priorityFeeInSol);
+
+            if (!isSimulation) {
+                const signature = await sendTransaction(connection, transaction, [payer]);
+                console.log('Sell transaction confirmed:', signature);
+            } else if (isSimulation) {
+                const simulatedResult = await connection.simulateTransaction(transaction);
+                console.log(simulatedResult);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
     async buy(
         privateKey: string,
         tokenAddress: string,
@@ -86,6 +241,7 @@ export default class PumpFunTrader {
     }
     async createAndSendTransaction(txBuilder: Transaction, privateKey: string, priorityFee: number = 0, isSimulation: boolean = true) {
         const walletPrivateKey = await getKeyPairFromPrivateKey(privateKey);
+
         const transaction = await createTransaction(this.connection, txBuilder.instructions, walletPrivateKey.publicKey, priorityFee);
         if (isSimulation == false) {
             const signature = await sendTransaction(this.connection, transaction, [walletPrivateKey]);
